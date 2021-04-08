25874f44592ae7d05fbb07660ea5e0c85049b3e1
Dilan <duriostegui@asgbpm.com>
2021-04-08 16:53:51 -0500

    Agregando columnas y estilos

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,16 +2,25 @@ import 'package:flutter/material.dart';
 
 
 class HomePage extends StatelessWidget{
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Titutlo'),
-          centerTitle: true,
-          elevation: 1.0,
-        ),
-        body: Center(
-          child: Text('Hola Mundo'),),
-      );
-    }
+  
+  final estiloTexto = new TextStyle(fontSize: 25);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titutlo'),
+        centerTitle: true,
+        elevation: 1.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Número de taps:',style: estiloTexto),
+            Text('0',style: estiloTexto),
+          ],
+        )
+      ),
+    );
+  }
 }
